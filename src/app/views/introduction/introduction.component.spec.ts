e762f6d3b01aba969bae8923d47c807481940b43
jamesbirrellgray <james@jamesbirrellgray.com>
2017-05-19 12:03:33 +0100

    chore: 4 unit tests failing, will investigate

--- src/app/views/introduction/introduction.component.spec.ts
+++ src/app/views/introduction/introduction.component.spec.ts
@@ -22,4 +22,17 @@ describe('IntroductionComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as title 'Make a plea for an offence'`, async(() => {
+    const fixture = TestBed.createComponent(IntroductionComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.appTitle).toEqual('Make a plea for an offence');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(IntroductionComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Make a plea for an offence');
+  }));
 });
